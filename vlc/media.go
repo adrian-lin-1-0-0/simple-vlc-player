eb9f7f6d15b2abd10ed49fc3e86def8db865566f
adrian-lin-1-0-0 <adrian.lin.1.0.0@gmail.com>
2023-05-07 11:48:11 +0800

    feat : support -f url

--- vlc/media.go
+++ vlc/media.go
@@ -16,12 +16,32 @@ type Media struct {
 
 func (i *Instance) NewMedia(path string) (*Media, error) {
 
+	if path[0:4] == "http" {
+		return i.NewMediaFromURL(path)
+	}
+	return i.NewMediaFromPath(path)
+}
+
+func (i *Instance) NewMediaFromPath(path string) (*Media, error) {
+
 	cPath := C.CString(path)
 	defer C.free(unsafe.Pointer(cPath))
 	media := C.libvlc_media_new_path(i.handle, cPath)
 
 	if media == nil {
 		return nil, errors.New("libvlc_media_new_path failed")
+	}
+	return &Media{media: media}, nil
+}
+
+func (i *Instance) NewMediaFromURL(url string) (*Media, error) {
+
+	cUrl := C.CString(url)
+	defer C.free(unsafe.Pointer(cUrl))
+	media := C.libvlc_media_new_location(i.handle, cUrl)
+
+	if media == nil {
+		return nil, errors.New("libvlc_media_new_location failed")
 	}
 	return &Media{media: media}, nil
 }
